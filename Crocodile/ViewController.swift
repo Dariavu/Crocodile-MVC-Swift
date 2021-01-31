275235653e833339de30a26cf0f918df444181d3
Serhii Rosovskyi <serhii.rosovskyi@spsoft.com>
2021-01-31 10:47:14 +0200

    Initial Commit

---
+++ Crocodile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crocodile
+//
+//  Created by Serhiy Rosovskyy on 31.01.2021.
+//  Copyright © 2021 Serhiy Rosovskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
